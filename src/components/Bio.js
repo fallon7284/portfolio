28b0116f1a24b9625b5e819236e847b0e8600ed4
Brendan Fallon <brendanc.fallon@gmail.com>
2019-09-16 13:05:26 -0400

    optimizes landing page image position for screen sizes/orientations, small change to comment button, change to bio content

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -28,13 +28,11 @@ export default () => {
                         </div>
                     </div>
                 </div>
-                <div>Not very long ago I was contributing my daily efforts to building skyscrapers rather than web applications, but in November of 2018 I took a week-long bootcamp prep course at Fullstack Academy and my life changed forever.</div>
-                <div>First it was () =>{'{}'}s, []s, and ""s. Then for (let i = 0)s, while (condition)s, and {'{}'}s.</div>
-                <div>The next thing I knew it was recursion, factory functions, classes, prototypal inheritance.</div>
-                <div>The prep course ended on a Sunday, and after 9 years in my field I gave my notice the next day.</div>
-                <div>I knew right away that I wanted this, I knew that I was good, and I couldn't wait.</div>
-                <div>2 weeks of studying later I was accepted into the next full-time immersive cohort at Fullstack.</div>
-                <div>5 more weeks and I was on the road to Chicago. <Link style={{textDecoration: 'none'}} to="/projects"><b className="link-bio-projects" >Check out some of the projects I built there and since!</b></Link></div>
+                <div>Not very long ago I was contributing my daily efforts to building skyscrapers rather than web applications, but in December of 2018, after completing a one-week bootcamp prep course, I decided that I wanted to change my life by changing my career path, and I enrolled in the full-time immersive program at Fullstack Academy.</div>
+                <br/>
+                <div>I love building software that is both functional and beautiful.  From <span>API</span> design to reusable <span>React</span> components to complex <span>state management</span>, I am focused and driven to master the technologies that underpin visually appealing, fast, and functional software.</div>
+                <br />
+                <div><Link style={{textDecoration: 'none'}} to="/projects"><b className="link-bio-projects" >Check out some of my work!</b></Link></div>
                 
             </div>
         </div>
