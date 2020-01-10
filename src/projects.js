a09cf22de78ef35a239b2fcbefd907abc31514c7
Brendan Fallon <brendanc.fallon@gmail.com>
2020-01-10 11:02:27 -0500

    adds swinesweeper game

--- src/projects.js
+++ src/projects.js
@@ -2,51 +2,64 @@ module.exports = [
     {
         name: 'Pixalive',
         image: require('../src/images/pixalive.png'),
-        description: 'Pixalive is a free, multi-user, real-time editor' +
-        ' for creating animated sprites and pixel art.  It was built with' +
-        ' only functional components using React-Hooks ' +
-        'along with Socket.io, HTML Canvas, and PostgreSQL.',
+        description:
+            'Pixalive is a free, multi-user, real-time editor' +
+            ' for creating animated sprites and pixel art.  It was built with' +
+            ' only functional components using React-Hooks ' +
+            'along with Socket.io, HTML Canvas, and PostgreSQL.',
         deployed: 'https://pixalive.herokuapp.com',
         github: 'https://github.com/pixalive/pixalive',
     },
     {
         name: 'Explore Outdoors',
         image: require('../src/images/explore-screen.png'),
-        description: 'A personal project, Google Maps- and Mountain Project-based React application along with Node/Express backend finds a user\'s location (or takes user input as address or location name and centers map to that location) and displays nearby hikes, climbs, and camping areas.',
+        description:
+            "A personal project, Google Maps- and Mountain Project-based React application along with Node/Express backend finds a user's location (or takes user input as address or location name and centers map to that location) and displays nearby hikes, climbs, and camping areas.",
         deployed: 'http://explore-outdoors.herokuapp.com',
-        github: 'http://github.com/fallon7284/explore-outdoors'
+        github: 'http://github.com/fallon7284/explore-outdoors',
     },
     {
         name: 'CatOrNah?',
         image: require('../src/images/catornah.jpg'),
-        description: 'For a second project at Fullstack Academy,' + 
-        ' students were challenged to learn and implement a new technology during a' +
-        ' four-day solo project.  CatOrNah? is a React Native ' +
-        ' camera application using the Clarifai image-recognition API to allow a user' +
-        ' to snap a photo and identify the subject as either a cat... or nah.',
+        description:
+            'For a second project at Fullstack Academy,' +
+            ' students were challenged to learn and implement a new technology during a' +
+            ' four-day solo project.  CatOrNah? is a React Native ' +
+            ' camera application using the Clarifai image-recognition API to allow a user' +
+            ' to snap a photo and identify the subject as either a cat... or nah.',
         deployed: null,
         github: 'https://github.com/fallon7284/catornah-',
     },
     {
+        name: 'Swine Sweeper',
+        image: require('../src/images/SwineSweeper.png'),
+        description: 'Pig-Themed Minesweeper Game.',
+        deployed: 'https://sharp-cray-74c6dd.netlify.com/',
+        github: 'http://github.com/fallon7284/swine-sweeper',
+    },
+    {
         name: 'Grace Shopper',
         image: require('../src/images/graceShopper.png'),
-        description: 'The first team project from my time at Fullstack Academy,' +  
-        'Grace Shopper was a Full Stack mock e-commerce web application.',
+        description:
+            'The first team project from my time at Fullstack Academy,' +
+            'Grace Shopper was a Full Stack mock e-commerce web application.',
         deployed: 'https://sellinbags.herokuapp.com',
         github: 'https://github.com/graceshoppingbags/sellinbags',
     },
     {
         name: 'This Portfolio Site!',
         image: require('../src/images/projects.png'),
-        description: 'This site was created with Create React App along with a Node/Express backend to allow for the comments section' +  
-        ' which includes the ability to reply to comments. Style is almost entirely CSS other than the top bar component and the message form inputs.',
+        description:
+            'This site was created with Create React App along with a Node/Express backend to allow for the comments section' +
+            ' which includes the ability to reply to comments. Style is almost entirely CSS other than the top bar component and the message form inputs.',
         // deployed: 'https://sellinbags.herokuapp.com',
         github: 'https://github.com/fallon7284/portfolio',
     },
     {
         name: 'Like what you see?',
         image: require('../src/images/happy.gif'),
-        description: 'I\'d love to hear from you.  Click contact above to leave a comment or to send me an email.',
-        github: 'http://github.com/fallon7284'
-    }
+        description:
+            "I'd love to hear from you.  Click contact above to leave a comment or to send me an email.",
+        github: 'http://github.com/fallon7284',
+    },
 ]
